52721b10812ca3726d3813558fa7cd2fa2447a90
Will Scerbo <will.scerbo.789@gmail.com>
2021-03-21 14:03:25 -0400

    Rewrote toggle guideline in FrameworkBuilder using state

--- src/pages/framework-builder/FrameworkBuilder.js
+++ src/pages/framework-builder/FrameworkBuilder.js
@@ -7,20 +7,78 @@ import './FrameworkBuilder.css';
 
 class FrameworkBuilder extends React.Component {
 
-    componentDidMount() {
-        [...document.getElementById("guidelineList").getElementsByTagName("li")].forEach(gl => gl.addEventListener("click", () => {
-            document.querySelector(".selectedGuideline").classList.toggle("guideline");
-            document.querySelector(".selectedGuideline").classList.toggle("selectedGuideline");
-            gl.classList.toggle("selectedGuideline");
-            gl.classList.toggle("guideline");
+    constructor(props) {
+        super(props);
+        this.state = {
+            frameworkName: "",
+            guidelines: [{
+                name: "New Guideline 1",
+                content: "Insert Content here",
+                selected: true
+            }]
+        }
+    }
 
-            //To do: swap the actual guideline area (guidelineEdit) when changing from guideline to guideline. 
-            //Make a component (???) for guidelines and have an array of them stored in the FrameworkBuilder state, and change which one 
-            //displays in this function
-        }));
+    componentDidMount() {
+        // [...document.getElementById("guidelineList").getElementsByTagName("li")].forEach(gl => gl.addEventListener("click", () => {
+        //     document.querySelector(".selectedGuideline").classList.toggle("guideline");
+        //     document.querySelector(".selectedGuideline").classList.toggle("selectedGuideline");
+        //     gl.classList.toggle("selectedGuideline");
+        //     gl.classList.toggle("guideline");
+
+        //     //To do: swap the actual guideline area (guidelineEdit) when changing from guideline to guideline. 
+        //     //Make a component (???) for guidelines and have an array of them stored in the FrameworkBuilder state, and change which one 
+        //     //displays in this function
+        // }));
+    }
+
+
+    //add guideline
+    addGuideline() {
+        let numGls = this.state.guidelines.length;
+        let newGuideline = {name: `New Guideline ${numGls + 1}`, content: "", selected: true};
+        //add guideline to state
+        this.setState({
+            guidelines: this.state.guidelines.concat(newGuideline)
+        });
+        //make sure the new guideline is selected
+        let glList = [...document.getElementById("guidelineList").getElementsByTagName("li")];
+        glList[glList.length - 1].classList.toggle("selectedGuideline");
+        glList[glList.length - 1].classList.toggle("guideline");
+    }
+
+
+    //Toggle currently selected guideline with state 
+    selectGuideline(event) {
+        let temp = this.state.guidelines;
+        temp.forEach(element => {
+            element.selected = false;
+          });
+        this.setState(({guidelines}) => ({guidelines: temp}));
+        let stateIndex = 0;
+        for (let j = 0; j < this.state.guidelines.length; j++) {
+            if (this.state.guidelines[j].name == event.target.firstChild.nodeValue) {
+                stateIndex = j;
+            }
+        }
+        let temp2 = this.state.guidelines;
+        for (let i = 0; i < temp2.length; i++) {
+            if (i == stateIndex) {
+                temp2[i].selected = true;
+            } else {
+                temp2[i].selected = false;
+            }
+        }
+        this.setState((guidelines) => ({guidelines: temp2}));
+        event.target.classList.toggle("selectedGuideline");
+    }
+
+    handleGuidelineChange() {
+
     }
 
     render() {
+
         return [
             <Header />, 
             <main id = "frameworkBuilder">
@@ -34,17 +92,20 @@ class FrameworkBuilder extends React.Component {
                     <div id = "guidelineArea">
                         <div className = "guidelines">
                             <ul id = "guidelineList">
-                                <li className = "selectedGuideline">Guideline 1</li>
-                                <li className = "guideline">Guideline 2</li>
-                                <li className = "guideline">Guideline 3</li>
+                                { 
+                                    this.state.guidelines.map(gl => 
+                                            <li onClick = {(event) => this.selectGuideline(event)}className = {gl.selected ? "selectedGuideline" : "guideline"}>{gl.name}</li>    
+                                    )
+                                }
+                                
                             </ul>
-                            <button className = "guidelineButton" id = "addGuideline">+ Add Guideline</button>
+                            <button type = "button" onClick = {() => this.addGuideline()} className = "guidelineButton" id = "addGuideline">+ Add Guideline</button>
                         </div>
                         <div className = "guidelineEdit">
                                 <label className = "glSectionLabel" for = "guidelineName">Guideline Name</label><br />
                                 <input id = "guidelineName" type = "text"/><br /><br />
                                 <label className = "glSectionLabel" for = "guidelineContents">Guideline Contents</label><br />
-                                <textarea id = "guidelineContents" /><br /><br />
+                                <textarea onChange = {() => this.handleGuidelineChange} id = "guidelineContents" /><br /><br />
                                 <button id = "removeGuideline" className = "guidelineButton">Remove </button>
                                 <button id = "saveGuideline" className = "guidelineButton">Save</button>                            
                         </div>
